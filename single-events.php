d76d6c2ccb5f8d343389cec49c3d68f1fe70cfca
Tori Holcomb <vgholcom@syr.edu>
2014-07-11 12:39:37 -0400

    events single

--- single-events.php
+++ single-events.php
@@ -6,14 +6,12 @@ get_header();
 if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<h1><?php the_title(); ?></h1><?php 
 	$id = get_the_ID();
-	echo get_the_post_thumbnail($id, 'medium', array('class'=>'img-responsive'));?>
+	echo get_the_post_thumbnail($id, 'large', array('class'=>'img-responsive'));?>
 	<i><?php 
-		$event_location = get_post_meta( $post->ID, '_event_location', true ); ?>
-		<div class="content"><?php
-				if (isset($event_location) && $event_location !='') { echo 'At '.$event_location.' on ';}
-				eventposttype_get_the_event_date('_start'); ?></i>
-			<?php the_content(); ?>
-		</div><php
+		$event_location = get_post_meta( $post->ID, '_event_location', true );
+		if (isset($event_location) && $event_location !='') { echo 'At '.$event_location.' on ';}
+		eventposttype_get_the_event_date('_start'); ?></i>
+	<?php the_content();
 endwhile; else: ?>
 	<p><?php _e('Sorry, no posts matched your criteria.'); ?></p><?php 
 endif;
